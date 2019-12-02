9cb816458e81e067d4b9e7fbe80928035cbca744
Niek <git@niekvantoepassing.nl>
2019-12-02 19:26:09 +0100

    Part 2 of day 1

--- day1.py
+++ day1.py
@@ -1,8 +1,20 @@
 import math
-
 fuel = 0
+
+def fuelRequired(mass):
+    return math.floor(mass / 3) - 2
+
+def addFuelForFuel(f):
+    global fuel
+    fuelWeight = fuelRequired(f)
+    if fuelWeight > 0:
+        fuel += fuelWeight
+        addFuelForFuel(fuelWeight)
+
 f = open("day1-input-a.txt", "r")
 for x in f:
-    fuel += math.floor(int(x) / 3)-2
+    fr = fuelRequired(int(x))
+    fuel += fr
+    addFuelForFuel(fr)
 
 print(fuel)
