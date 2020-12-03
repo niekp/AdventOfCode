532ca5bb01bb9eb69ae37cf785ed90aa6b6566b7
niekp <git@niekvantoepassing.nl>
2020-12-03 16:58:57 +0100

    Some setup for day 3

--- 2020/day3/Program.cs
+++ 2020/day3/Program.cs
@@ -1,6 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
-var input = File.ReadAllText("input.txt");
+namespace Day3 {
+	class Map {
+		private string[] Lines { get; set; }
+		private int X { get; set; }
+		private int Y { get; set; }
+		private int SpeedX { get; }
+		private int SpeedY { get; }
+		private List<(string Character, int Count)> Occurrences { get; set; }
 
-Console.WriteLine(input);
+		public Map(string Input, int SpeedX, int SpeedY) {
+			Lines = Input.Split(Environment.NewLine);
+			this.SpeedX = SpeedX;
+			this.SpeedY = SpeedY;
+
+			X = 0;
+			Y = 0;
+		}
+
+		
+
+	}
+
+	class Program {
+		static void Main(string[] args) {
+			var map = new Map(File.ReadAllText("input.txt"), 3, 1);
+
+
+		}
+	}
+
+	
+
+}
