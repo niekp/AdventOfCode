1d727b76fb6e5bec999a5588402f5229be1df752
Niek <git@niekvantoepassing.nl>
2021-06-16 12:21:09 +0200

    Day 5 part 2

--- 2020/day5.php
+++ 2020/day5.php
@@ -1,27 +1,106 @@
 <?php
 
-function getSeatHashcode($input) {
-    $rows = range(0, 127);
-    $seats = range(0, 7);
+class Plane
+{
+    private array $seats = [];
+    private array $rows;
+    private array $columns;
 
-    for ($i = 0; $i < strlen($input); $i++) {
-        $char = ($input[$i]);
+    public function __construct($rows, $columns)
+    {
+        $this->rows = range(0, $rows);
+        $this->columns = range(0, $columns);
 
-        if ($char == 'F') {
-            $rows = array_slice($rows, 0, count($rows) / 2);
-        } else if ($char == 'B') {
-            $rows = array_slice($rows, count($rows) / 2);
-        } else if ($char == 'R') {
-            $seats = array_slice($seats, count($seats) / 2);
-        } else if ($char == 'L') {
-            $seats = array_slice($seats, 0, count($seats) / 2);
+        foreach ($this->rows as &$row) {
+            foreach ($this->columns as &$column) {
+                $this->seats[] = new Seat($row, $column);
+            }
         }
     }
 
-    return $rows[0] * 8 + $seats[0];
+    public function occupySeat($input): void
+    {
+        $rows = $this->rows;
+        $columns = $this->columns;
+
+        for ($i = 0; $i < strlen($input); $i++) {
+            $char = ($input[$i]);
+
+            if ($char == 'F') {
+                $rows = array_slice($rows, 0, count($rows) / 2);
+            } else if ($char == 'B') {
+                $rows = array_slice($rows, count($rows) / 2);
+            } else if ($char == 'R') {
+                $columns = array_slice($columns, count($columns) / 2);
+            } else if ($char == 'L') {
+                $columns = array_slice($columns, 0, count($columns) / 2);
+            }
+        }
+
+        $matchingSeats = array_filter($this->seats, fn ($seat) => $seat->row == $rows[0] && $seat->column == $columns[0]);
+        array_walk($matchingSeats, fn ($seat) => $seat->full = true);
+    }
+
+    public function getHighestId(): int
+    {
+        $full_seats = $this->getFilteredSeats(true);
+        usort($full_seats, function ($a, $b) {
+            return $b->seat_id() - $a->seat_id();
+        });
+        return $full_seats[0]->seat_id();
+    }
+
+    private function getFilteredSeats($full = false): array
+    {
+        return array_filter($this->seats, fn ($seat) => $seat->full == $full);
+    }
+
+    private function seat_id_full($id): bool
+    {
+        return !!array_filter($this->seats, fn ($seat) => $seat->full && $seat->seat_id() == $id);
+    }
+
+    public function getEmptySeat(): Seat
+    {
+        $empty_seats = $this->getFilteredSeats();
+
+        $filtered_seats = array_filter(
+            $empty_seats,
+            fn ($seat) => $this->seat_id_full($seat->seat_id() - 1) && $this->seat_id_full($seat->seat_id() + 1)
+        );
+
+        return reset($filtered_seats);
+    }
+}
+
+class Seat
+{
+    public int $row;
+    public int $column;
+    public bool $full = false;
+
+    public function __construct($row, $column)
+    {
+        $this->row = $row;
+        $this->column = $column;
+    }
+
+    public function seat_id(): int
+    {
+        return $this->row * 8 + $this->column;
+    }
 }
 
 $input = explode("\n", file_get_contents("input/day5.txt"));
-$codes = array_map('getSeatHashcode', $input);
-rsort($codes);
-print_r($codes[0]);
+
+$plane = new Plane(127, 7);
+foreach ($input as $seat) {
+    $plane->occupySeat($seat);
+}
+
+print_r(
+    [
+        'Highest occupied seat' => $plane->getHighestId(),
+        'Empty seat' => $plane->getEmptySeat()->seat_id(),
+    ],
+);
