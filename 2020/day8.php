c3e524a59a3d630d13318c21831ae4b8d0d632fd
Niek <git@niekvantoepassing.nl>
2021-06-17 11:22:56 +0200

    Rename variables and add some comments

--- 2020/day8.php
+++ 2020/day8.php
@@ -2,25 +2,30 @@
 class Program {
     private int $pointer = 0;
     private int $accumulator = 0;
-    private array $executed = array(); // history
+    private array $history = array();
 
     public function __construct($commands)
     {
         $this->commands = $commands;
     }
-
-    public function run() {
+    
+    /**
+     * Run the program. 
+     *
+     * @return array | [bool error, int accumulator]
+     */
+    public function run(): array {
         $error = false;
         while (true) {
             // Break if the command was already visited or is out of bounds
             if (count($this->commands) - 1 < $this->pointer) {
                 break;
-            } else if (in_array($this->pointer, $this->executed)) {
+            } else if (in_array($this->pointer, $this->history)) {
                 $error = true;
                 break;
             }
             // Keep track of the history
-            $this->executed[] = $this->pointer;
+            $this->history[] = $this->pointer;
 
             // Get the command and execute it
             $command = $this->commands[$this->pointer];
@@ -46,34 +51,62 @@ class Program {
     }
 }
 
-function debug_program($commands) {
-    $original_commands = (new ArrayObject($commands))->getArrayCopy();
-
-    // JMP => NOP
-    foreach (array_filter($commands, fn($c) => $c['command'] == 'jmp') as $key => &$command) {
-        $try_commands = (new ArrayObject($original_commands))->getArrayCopy();
-
-        $try_commands[$key]['command'] = 'nop';
-
-        $program = new Program($try_commands);
-        if (!$program->run()['error']) {
-            return $program->run()['accumulator'];
-        }
+class Debugger {
+    private array $commands;
+    public function __construct(array $commands)
+    {
+        $this->commands = $commands;
     }
-
-    // NOP => JMP
-    foreach (array_filter($commands, fn($c) => $c['command'] == 'nop') as $key => &$command) {
-        $try_commands = (new ArrayObject($original_commands))->getArrayCopy();
-
-        $try_commands[$key]['command'] = 'jmp';
-
-        $program = new Program($try_commands);
-        if (!$program->run()['error']) {
-            return $program->run()['accumulator'];
+    
+    /**
+     * Get a copy of the commands to alter
+     *
+     * @return array
+     */
+    private function getCommandsCopy(): array {
+        return (new ArrayObject($this->commands))->getArrayCopy();
+    }
+    
+    /**
+     * Try to swap each commandA for commandB (one at a time).
+     * Check if this results in a combination that doesn't give an error
+     *
+     * @param  string $commandA
+     * @param  string $commandB
+     * @return int
+     */
+    private function trySwap(string $commandA, string $commandB): ?int {
+        foreach (array_filter($this->commands, fn($c) => $c['command'] == $commandA) as $key => &$command) {
+            $try_commands = $this->getCommandsCopy();
+    
+            $try_commands[$key]['command'] = $commandB;
+    
+            $program = new Program($try_commands);
+            if (($output = $program->run())
+                && !$output['error']) {
+                return $output['accumulator'];
+            }
         }
+        return null;
+    }
+    
+    /**
+     * Debug and correct the input and return a valid output.
+     *
+     * @return void
+     */
+    public function debug() {
+        if ($output = $this->trySwap('jmp', 'nop')) {
+            return $output;
+        }
+        if ($output = $this->trySwap('nop', 'jmp')) {
+            return $output;
+        }
+        return null;
     }
 }
 
+// Split the output into a 'command' array
 $input = explode("\n", file_get_contents("input/day8.txt"));
 $commands = array_map(function($command) {
     $splitted = explode(' ', $command);
@@ -83,5 +116,5 @@ $commands = array_map(function($command) {
     ];
 }, $input);
 
-
-print_r(debug_program($commands));
+$debugger = new Debugger($commands);
+print_r($debugger->debug());
