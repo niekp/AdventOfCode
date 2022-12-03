d65cf4c0c6173da02165ca4babbfe5d58468cb29
Niek Pijp <git@niekvantoepassing.nl>
2022-12-03 11:25:39 +0100

    day 3 b

--- 2022/day3.php
+++ 2022/day3.php
@@ -1,5 +1,13 @@
 <?php
 
+class prioritizer
+{
+	public static function prioritize(string $item): int
+	{
+		return $item > 'Z' ? ord($item) - 96 : ord($item) - 38;
+	}
+}
+
 class rucksack
 {
 	private array $compartment1;
@@ -8,36 +16,67 @@ class rucksack
 
 	public function __construct($input)
 	{
-		if (strlen($input) % 2 !== 0) {
-			throw new \InvalidArgumentException('Invalid number of items in rucksack');
-		}
-
-		$this->compartment2 = str_split($input);
-		$this->compartment1 = array_splice($this->compartment2, 0, strlen($input) / 2);
+		[$this->compartment1, $this->compartment2] = array_map(
+			static fn($p) => str_split($p), // Split compartment into parts
+			str_split($input, strlen($input) / 2) // Split in 2
+		);
 	}
 
 	public function part1(): int
 	{
 		$intersect = array_unique(array_intersect($this->compartment1, $this->compartment2));
-		return array_reduce($intersect, function ($carry, $item) {
-			return $carry + $this->getPriority($item);
+
+		return array_reduce($intersect, static function ($carry, $item) {
+			return $carry + prioritizer::prioritize($item);
 		}, 0);
 	}
 
-	private function getPriority($item): int
+	public function getItems(): array
 	{
-		$priority = ord($item);
-		return $priority > 90 ? $priority - 96 : $priority - 38;
+		return array_merge($this->compartment1, $this->compartment2);
 	}
 }
 
-$rucksacks = array_filter(explode("\n", file_get_contents("input/day3.txt")));
+class inspector
+{
+	private array $rucksacks;
 
-$part1 = array_reduce($rucksacks, function ($carry, $rucksack) {
-	$sack = new rucksack($rucksack);
-	return $carry + $sack->part1();
+	public function __construct(array $rucksacks)
+	{
+		$this->rucksacks = $rucksacks;
+	}
+
+	public function findBadge()
+	{
+		$badges = array_unique(
+			array_intersect(
+				...array_map(static fn(rucksack $rucksack) => $rucksack->getItems(),
+					$this->rucksacks)
+			)
+		);
+
+		return reset($badges);
+	}
+}
+
+$rucksacks = array_map(
+	static fn($rucksack) => new rucksack($rucksack),
+	array_filter(explode("\n", file_get_contents("input/day3.txt")))
+);
+
+// Part 1
+$part1 = array_reduce($rucksacks, static fn($carry, rucksack $rucksack) => $carry + $rucksack->part1(), 0);
+
+// Part 2
+$groups = array_chunk($rucksacks, 3);
+$inspectors = array_map(static fn($group) => new inspector($group), $groups);
+$badges = array_map(static fn(inspector $inspector) => $inspector->findBadge(), $inspectors);
+
+$part2 = array_reduce($badges, static function ($carry, $badge) {
+	return $carry + prioritizer::prioritize($badge);
 }, 0);
 
 print_r([
 	'part 1' => $part1,
+	'part 2' => $part2,
 ]);
