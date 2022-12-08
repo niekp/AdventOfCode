c7d134f87207f2ca0ae639a7263470a00a147811
Niek Pijp <git@niekvantoepassing.nl>
2022-12-08 20:43:06 +0100

    day 6 b

--- 2022/day6.php
+++ 2022/day6.php
@@ -2,22 +2,29 @@
 
 $input = file_get_contents("input/day6.txt");
 
-$buffer = [];
-foreach (str_split($input) as $index => $char) {
-	$buffer[] = $char;
-	$buffer_length = count($buffer);
+function getStartMarker(string $input, $marker): int
+{
+	$buffer = [];
+	foreach (str_split($input) as $index => $char) {
+		$buffer[] = $char;
+		$buffer_length = count($buffer);
 
-	if ($buffer_length < 4) {
-		continue;
-	}
-	if ($buffer_length > 4) {
-		array_shift($buffer);
+		if ($buffer_length < $marker) {
+			continue;
+		}
+		if ($buffer_length > $marker) {
+			array_shift($buffer);
+		}
+
+		if (count(array_unique($buffer)) === $marker) {
+			return ($index + 1);
+		}
 	}
 
-	if (count(array_unique($buffer)) === 4) {
-		print_r([
-			'part 1' => $index + 1,
-		]);
-		break;
-	}
+	return 0;
 }
+
+print_r([
+	'part 1' => getStartMarker($input, 4),
+	'part 2' => getStartMarker($input, 14),
+]);
